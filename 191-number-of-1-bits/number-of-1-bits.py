d6db6cb2a575f300f3ccfe1017d4a7c2cde5cdb6
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-08-12 19:17:10 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (31.60%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,14 +1,13 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         bin_cov=[]
+        count=0
         while n > 0:
             remainder = n % 2
             bin_cov.append(str(remainder))
             n //= 2
+            count=count+remainder 
         x="".join(bin_cov[::-1])
-        count=0
-        for i in x:
-            if i=='1':
-                count=count+1
         return count
-
+        
+        
