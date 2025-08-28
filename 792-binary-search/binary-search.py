6876389b23dfdd91888f7581a40615a51d60f9e2
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-08-28 10:27:18 -0600

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.25%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,7 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i]==target:
-                return i
-        else:
-            return -1
+        l,r=0,len(nums)-1
+      
+        while l<=r:
+            m=(l+r)//2
+            if nums[m]>target:
+                r=m-1
+            elif nums[m]<target:
+                l=m+1
+            else: 
+                return m
+        return -1
+            
