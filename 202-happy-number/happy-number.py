9cc413e5d9d161d8690712f1d3c01d0a17b5ce4e
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-09-12 10:55:33 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.23%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,18 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        v=set()
-        while n not in v:
-            v.add(n)
-            n=self.sumofsquares(n)
-            if n==1:
+        visit = set()
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+            if n == 1:
                 return True
         return False
-    def   sumofsquares(self, n: int)  -> int:  
-        out=0
-        while n!=0:
-            d=n%10
-            d=d**2
-            out+=d
-            n=n//10
-        return out
 
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            output += digit
+            n = n // 10
+        return output
