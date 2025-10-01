218b4d41a77597f0baf37649cf3c50d1b911df51
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-09-30 18:09:09 -0600

    Time: 48 ms (85.17%) | Memory: 21 MB (56.67%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        q_copy = q
+        p_copy = p
+        while q_copy != p_copy:
+            q_copy = q_copy.parent if q_copy else p
+            p_copy = p_copy.parent if p_copy else q
+        return q_copy
+
+    
