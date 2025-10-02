d731aec73092a887c6255052fb8a21b73aaa6ede
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-10-02 13:21:22 -0600

    Time: 0 ms (100.00%) | Memory: 19.1 MB (53.42%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,14 +9,15 @@ class Solution:
         i = -1
         while l <= r:
             m = (l+r)//2
-            if target > nums[m]:
-                l=m+1
-            elif target < nums[m]:
-                r=m-1
-            else:
+            if nums[m] ==target:
                 i=m
                 if leftBias:
                     r= m-1
                 else:
                     l=m+1
+            elif nums[m] > target :
+                r=m-1
+            else:
+                l = m+1
+                
         return i 
