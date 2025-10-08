d9774855e11507db751316be8dbfa43b09c42186
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-10-08 08:20:05 -0600

    Time: 27 ms (95.42%), Space: 18.2 MB (20.03%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l , r = 0 , len(s)-1
+        l , r = 0 , len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                SkipL , SkipR= s[l+1:r+1], s[l:r]
-                return (SkipL==SkipL[::-1] or SkipR == SkipR[::-1])
+                SkipL, SkipR = s[l+1:r+1],s[l:r]
+                return  SkipL==SkipL[::-1] or SkipR==SkipR[::-1]
             l +=1
-            r -=1
+            r-=1
+           
         return True
-    
+
+
         #Time complexity: O(n)
         #Space complexity: O(1)
