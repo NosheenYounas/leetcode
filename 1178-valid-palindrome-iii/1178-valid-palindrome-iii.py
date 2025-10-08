cca9f0ee3690cf6cc099b9926b43088341794318
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-10-08 12:19:50 -0600

    Time: 136 ms (89.03%), Space: 19.5 MB (85.31%) - LeetHub

--- 1178-valid-palindrome-iii/1178-valid-palindrome-iii.py
+++ 1178-valid-palindrome-iii/1178-valid-palindrome-iii.py
@@ -1,35 +1,25 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-        self.string = s
-        if not k: 
-            return self.is_Palindrome(0,len(s)-1)
-        memo = {}
-
-        def helper(i,j, k):
-            if (i,j,k) in memo:
-                return memo[(i,j,k)]
-            elif not k: 
-                memo[(i,j,k)] = self.is_Palindrome(i,j)
-            else:
-                while i<j:
-                    if self.string[i] != self.string[j]:
-                        memo[(i,j,k)] = helper(i+1,j,k-1) or helper(i,j-1,k-1)
-                        return memo[(i,j,k)]
-                    i += 1
-                    j -= 1
-                memo[(i,j,k)] = True
-            return memo[(i,j,k)]
-        return helper(0,len(self.string)-1,k)
-
-
-
-    
-    def is_Palindrome(self,i , j):
-        while i < j:
-            if self.string[i] != self.string[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
+        n = len(s)
+        # Create a 2D DP table initialized with zeros
+        memo = [[0] * n for _ in range(n)]
+        
+        # Generate all combinations of `i` and `j` in the correct order.
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                # Case 1: Character at `i` equals character at `j`
+                if s[i] == s[j]:
+                    memo[i][j] = memo[i + 1][j - 1]
+                # Case 2: Character at `i` does not equal character at `j`.
+                # Either delete character at `i` or delete character at `j`
+                # and try to match the two pointers using recursion.
+                # We need to take the minimum of the two results and add 1
+                # representing the cost of deletion.
+                else:
+                    memo[i][j] = 1 + min(memo[i + 1][j], memo[i][j - 1])
+        
+        # Return true if the minimum cost to create a palindrome by only deleting the letters
+        # is less than or equal to `k`.
+        return memo[0][n - 1] <= k
 #T  O(n) * O(n)
 #S O(n)* O(n)
