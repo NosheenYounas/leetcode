ec05170d2d801296ed228294370df193866d7f55
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2025-10-09 19:18:02 -0600

    Time: 4 ms (48.96%) | Memory: 25 MB (97.48%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -6,16 +6,18 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
         if not root:
             return 0
-
-        if  root.val > high:
+        if root.val < low:
+            return self.rangeSumBST(root.right,low,high)
+        if root.val > high:
             return self.rangeSumBST(root.left,low,high)
+        return root.val +self.rangeSumBST(root.right,low,high)+self.rangeSumBST(root.left,low,high)
 
-        if root.val < low :
-            return self.rangeSumBST (root.right, low, high)
-        
-        return  root.val+self.rangeSumBST(root.left,low,high) + self.rangeSumBST (root.right, low, high)
 
+    
+      #Time O(logn) for a balance tree worse O(n)
+      #Space O(n) recursive stack calls  
 
         
