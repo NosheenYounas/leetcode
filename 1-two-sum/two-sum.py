0fa34d37c1dc958c68ea6b2d1ef0ae259a2af0c5
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-06-25 07:02:21 -0700

    Time: 0 ms (100.00%) | Memory: 20.5 MB (41.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapp = {}
+        dick = {}
         for i,n in enumerate(nums):
-            diff = target - n
-            if diff in mapp:
-                return [mapp[diff],i]
-            mapp[n]=i
-        
+            k = target - n 
+            if k in dick:
+                return [dick[k],i]
+            dick[n] = i
