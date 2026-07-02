a16d458994d0682e1a2755f701e499c835c9b921
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-02 13:03:42 -0700

    Time: 0 ms (100.00%) | Memory: 20 MB (69.85%) - LeetSync

---
+++ 280-wiggle-sort/wiggle-sort.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n-1):
+            if i %2 == 0:
+                if nums[i] > nums[i+1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            else:
+                if nums[i] < nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        
+
+                
