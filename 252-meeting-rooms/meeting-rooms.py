a17b98e05e0da7d76c3bd800f84676e7996526c2
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-13 11:48:12 -0700

    Time: 2 ms (78.64%) | Memory: 21.3 MB (76.69%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,13 +1,14 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort(key = lambda x:x[0])
         for i in range(1, len(intervals)):
-            prev_end = intervals[i - 1][1]
-            curr_start = intervals[i][0]
-
-            # Overlap found
-            if prev_end > curr_start:
+            prev_end = intervals[i-1][1]
+            curr = intervals[i][0]
+            if prev_end > curr:
                 return False
         return True
+
+
+
 
         
