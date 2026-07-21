d9c4af83582666281dad6e6ee38448fce40bf846
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-21 09:14:52 -0700

    Time: 125 ms (67.31%) | Memory: 48.8 MB (75.38%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,19 +1,24 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        n = len(grid)
-        m = len(grid[0])
-        row = [0] * n
-        col = [0] * m
-        diff = [[0] * m for _ in range(n)]
+    
+        m = len(grid)
+        n = len(grid[0])
+        row_ones = [0] * m
+        col_ones = [0] * n
 
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
-                    row[i] += grid[i][j]
-                    col[j] += grid[i][j]
+                    row_ones[i] += 1
+                    col_ones[j] += 1
 
-        for i in range(n):
-            for j in range(m):
-                diff[i][j] = (2 * row[i] - n) + (2 * col[j] - m)
+        diff = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                ones_row_i = row_ones[i]
+                ones_col_j = col_ones[j]
+                zeros_row_i = n - ones_row_i
+                zeros_col_j = m - ones_col_j
+                diff[i][j] = ones_row_i + ones_col_j - zeros_row_i - zeros_col_j
 
         return diff
