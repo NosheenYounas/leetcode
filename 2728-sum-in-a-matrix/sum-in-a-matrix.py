c316e90fc3a337fdbc32038f898833aa7b964253
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-21 10:29:31 -0700

    Time: 52 ms (56.70%) | Memory: 34.5 MB (50.89%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def matrixSum(self, grid: List[List[int]]) -> int:
+        for row in grid:
+            row.sort()
+        n = len(grid[0])
+        ans = 0 
+        for j in range(n):
+            ans += max(row[j] for row in grid)
+        return ans
+        
+        
