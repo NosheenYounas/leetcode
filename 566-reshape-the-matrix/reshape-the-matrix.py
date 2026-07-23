a1f21be7eb153a66ae1deee07cbed379cffee604
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-22 17:13:48 -0700

    Time: 3 ms (29.95%) | Memory: 20 MB (11.90%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -1,8 +1,20 @@
 class Solution:
-    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
-        A = [x for row in nums for x in row ]
-        if r * c == len(A):
-            return [A[i*c : (i + 1)*c] for i in range(r) ]
-        else:
-            return nums
-        
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        col=len(mat[0])
+        row=len(mat)
+        temp=[]
+        result=[]
+
+        if row*col!=r*c:
+            return mat
+        for i in range(row):
+            for j in range(col):
+                temp.append(mat[i][j])
+        x=0
+        for i in range(r):
+            lil=[]
+            for j in range(c):
+                lil.append(temp[x])
+                x+=1  
+            result.append(lil)  
+        return result 
