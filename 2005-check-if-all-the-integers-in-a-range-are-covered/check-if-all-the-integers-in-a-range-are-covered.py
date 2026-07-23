0a605e4170061a704e86d3dffbac4cc8ebf047c1
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-07-23 15:07:49 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.29%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,9 +1,6 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-        slots =[0]*51
-        for s , e in ranges:
-            slots[s:e+1] = [1] *(e-s+1)
-        for i in range(left, right+1):
-            if slots[i]==0 :
-                return False
-        return True      
+        for s, e in sorted(ranges):
+            if s <= left <= e:
+                left = e + 1
+        return left > right     
