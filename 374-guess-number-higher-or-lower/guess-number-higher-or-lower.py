525cde41db4df9854634b481a9baa65ef450988c
Nosheen Younas <54187552+NosheenYounas@users.noreply.github.com>
2026-08-02 16:52:35 -0700

    Time: 39 ms (86.68%) | Memory: 19.2 MB (69.43%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -3,24 +3,20 @@
 # @return -1 if num is higher than the picked number
 #          1 if num is lower than the picked number
 #          otherwise return 0
-# def guess(num):
+# def guess(num: int) -> int:
 
-class Solution(object):
-    def guessNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-       
-        low = 1
-        high = n
-        
-        while True:
-            mid = low + (high - low) // 2
-            myGuess = guess(mid)
-            if myGuess == 1:
-                low = mid + 1
-            elif myGuess == -1:
-                high = mid - 1
-            else:
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 1 , n
+        while l <= r:
+            mid = (l+r)//2
+            result = guess(mid)
+            if result == 0:
                 return mid
+            elif result == -1:
+                r = mid -1
+            else:
+                l = mid+1
+                
+        
+        
